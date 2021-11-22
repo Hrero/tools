e049f5ab545aa6e26c0f3e4f4fe3130710dfa820
haozengrun <haozengrun@duiba.com.cn>
2021-11-22 16:40:55 +0800

    command

---
+++ bin/init.js
@@ -0,0 +1,11 @@
+/**
+ * 转换入口导出一个函数，按照如下函数签名
+ * @param {*} fileInfo 包含 source 和 path 属性。source为待转换文本，path为路径
+ * @param {*} api 包含 gogocode 作为转换工具
+ * @param {*} options 其他 option 由此传入
+ * @returns {string} 返回转换后的代码
+ */
+ module.exports = function(fileInfo, api, options) {
+    console.log('init');
+};
+  
--- bin/tbjd.js
+++ bin/tbjd.js
@@ -1,42 +1,35 @@
 #!/usr/bin/env node
-
 const program = require('commander');
+const term = require('terminal-kit').terminal;
+//插件加载及执行逻辑
+const transform = require('./transform');
+//初始化插件项目逻辑
+const init = require('./init');
 const pkg = require('../package.json');
-const message = require('../lib/message');
 
-// 检查 Node 版本
-if (Number.parseFloat(process.versions.node) < 8) {
-  message.error(
-    `You are using Node ${process.versions.node}, but this version requires Node 11 or higher\nPlease upgrade your Node version.`,
-  );
-  process.exit(1);
-}
-
+// 配置command
 program
-  // 处理显示版本命令，默认选项标识为-V和--version，当存在时会打印版本号并退出
-  .version(pkg.version, '-V, --version', 'output the current version')
+  .command('init')
+  .description('初始化一个插件项目')
+  .action((name) => {
+    init(name);
+  })
 
-  // 重写覆盖默认的帮助标识和描述
-  .helpOption('-h, --help', 'read more information')
+// 配置options
+program
+  .option('-t, --transform <package name or path>', '插件路径或者npm包名称,支持多个插件，逗号分隔')
+  .option('-o, --out <path>', '输出文件路径')
+  .option('-s, --src <path>', '需要转换的源文件路径')
+  .action((options) => {
+    console.log(1);
+      return transform(options);
+  });
 
-  // 定义选项，同时也用于这些选项的文档。每个选项可以有一个短标识(单个字符)和一个长名字，它们之间用逗号或空格分开
-  // 选项会被放到 Commander 对象的属性上，多词选项如"--template-engine"会被转为驼峰法program.templateEngine。多个短标识可以组合为一个参数，如-a -b -c等价于-abc
-  // 最常用的两个选项类型是boolean(选项后面不跟值)和选项跟一个值（使用尖括号声明）。除非在命令行中指定，否则两者都是undefined
-  .option('-d, --debug', 'output extra debugging')
-  .option('-p, --pizza-type <type>', 'flavour of pizza')
-  .option('-c, --cheese <type>', 'add the specified type of cheese', 'blue') // 指定默认值
-  .option('-e, --cheese [type]', 'Add cheese with optional type') // 指定一个用作标志的选项，它可以接受值（使用方括号声明，即传值不是必须的）
+// 配置 cli 信息，版本、cli说明等
+program
+  .version(pkg.version)
+  .description(term.blue('京东小工具'));
 
-  .usage('自定义在帮助信息第一行中显示的命令使用描述')
-
-  // 为最高层命令指定子命令，第一个参数可以指定命令的名字以及任何参数，参数可以是 <required>(必须的) or [optional](可选的)，第二个参数为命令的描述
-  // 当 .command() 带有描述参数时，不能采用 .action(callback) 来处理子命令，否则会出错。这告诉 commander，你将采用单独的可执行文件作为子命令，就像 git(1) 和其他流行的工具一样。 Commander 将会尝试在入口脚本（例如 ./lib/lzg）的目录中搜索 program-command 形式的可执行文件，例如 lzg-init, lzg-config
-  // 指定 opts.isDefault 为 true 将会在没有其它子命令指定的情况下，执行该子命令。例如 lzg  等价于 lzg init
-  .command('init <projectName>', '创建新项目', { isDefault: true }).alias('start')
-  .command('config [service]', 'stop named service, or all')
-
-  // 处理参数，没有被使用的选项会被存放在program.args数组中
+// 接管命令行输入，参数处理
+program
   .parse(process.argv);
-
-if (program.debug) console.log(program.opts());
-if (program.pizzaType) console.log(`- ${program.pizzaType}`);
